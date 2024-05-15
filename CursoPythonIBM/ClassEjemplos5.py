d1a5fbdb4d4ee2daf0de5f165204dba0bd65ccc8
JG <gutjes90@gmail.com>
2024-05-15 17:43:45 +0200

    Version One

--- CursoPythonIBM/ClassEjemplos5.py
+++ CursoPythonIBM/ClassEjemplos5.py
@@ -340,3 +340,38 @@ print(f'{fahrenheit} F a C: {resultado:0.2f}')
 
 
 
+# MODULOS
+
+"""
+Para llamar a una función que tengamos en un módulo tenemos que importarla y luego 
+usar la nomenclatura del punto:
+Suponiendo que en un módulo 018_Modulos.py tenemos la siguiente función suma:
+"""
+def suma(num1, num2):
+    print("suma: ", num1 + num2)
+
+#   Desde otro módulo la llamaríamos así:
+import funciones
+funciones.suma(3,4)     #Además de importar debemos usar la nomenclatura del punto.
+
+
+#   Para simplificar también podemos poner:
+
+from funciones import suma
+suma(3, 4)
+# De esta forma, además, ya no necesitamos usar la nomenclatura del punto.
+# ---------------------------------------
+
+# Si queremos importar todo el contenido de un paquete:
+from funciones import *
+
+# La función dir() enumera todos los elementos de un módulo. 
+import funciones
+
+x = dir(funciones)
+print(x)
+2400
+
+
+
+
